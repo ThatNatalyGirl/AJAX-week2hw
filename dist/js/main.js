8abdf9d36f18fa12eb6186a7455ec5faca78d95f
Nataly <ncgarzon88@gmail.com>
2018-04-18 15:58:10 -0400

    trial and error

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,28 @@
 "use strict";
 
-console.log("Hello World from app.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+console.log("I guess I work");
+
+var postButton = document.querySelector(".post-button button");
+
+postButton.addEventListener("click", function () {
+	console.log("check");
+});
+
+// $("button").click(function(e) {
+//     e.preventDefault();
+//     $.ajax({
+//         type: "POST",
+//         url: "/pages/test/",
+//         data: { 
+//             id: $(this).val(), // < note use of 'this' here
+//             access_token: $("#access_token").val() 
+//         },
+//         success: function(result) {
+//             alert('ok');
+//         },
+//         error: function(result) {
+//             alert('error');
+//         }
+//     });
+// });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
 console.log(`I guess I work`)
+
+
+var postButton = document.querySelector(".post-button button");
+
+postButton.addEventListener("click", function(){
+	console.log("check")
+});
+
+// $("button").click(function(e) {
+//     e.preventDefault();
+//     $.ajax({
+//         type: "POST",
+//         url: "/pages/test/",
+//         data: { 
+//             id: $(this).val(), // < note use of 'this' here
+//             access_token: $("#access_token").val() 
+//         },
+//         success: function(result) {
+//             alert('ok');
+//         },
+//         error: function(result) {
+//             alert('error');
+//         }
+//     });
+// });
